9be32fc7a22a0751aebe7ede53324aee7f1ca1bc
Milena Mayumi Nagano Da Silva <mimayumi@c3r7p1.42sp.org.br>
2024-10-29 14:40:32 -0300

    ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimayumi <mimayumi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 19:20:04 by mimayumi          #+#    #+#             */
+/*   Updated: 2024/10/29 14:38:33 by mimayumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dest && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}	
+	return (dest);
+}
