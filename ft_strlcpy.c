5c0566f2d5dea744b02747e9e090b3e928908fa8
Milena Mayumi Nagano Da Silva <mimayumi@c2r9p1.42sp.org.br>
2024-11-01 19:30:09 -0300

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimayumi <mimayumi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 16:21:40 by mimayumi          #+#    #+#             */
+/*   Updated: 2024/11/01 19:20:23 by mimayumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(src);
+	if (size == 0)
+		return (len);
+	i = 0;
+	while (src[i] && (i < (size -1)))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len);
+}
